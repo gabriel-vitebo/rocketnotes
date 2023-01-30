5cfd2a9166a75743b9cfe1c3586606a675e1d7b9
Gabriel Vitebo <vitebo@outlook.com>
2023-01-30 20:00:11 -0300

    creating the tags

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -3,6 +3,7 @@ import { Container, Links } from "./style"
 import { Header } from "../../componets/header"
 import { Button } from "../../componets/button"
 import { Section } from "../../componets/Section"
+import { Tag } from "../../componets/Tag"
 
 export function Details() {
   return (
@@ -11,12 +12,18 @@ export function Details() {
 
       <Section title="Links úteis">
         <Links>
-          <li>
-            <a href="@">link 1</a>
-          </li>
+          <li><a>https://www.rocketseat.com.br/</a></li>
+        </Links>
+        <Links>
+          <li><a>https://www.rocketseat.com.br/</a></li>
         </Links>
       </Section>
-      
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
+      </Section>
+
       <Button title="voltar" />
     </Container>
   )
