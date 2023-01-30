1039791668cf0295bb80a87000904c7049f63a5b
Gabriel Vitebo <vitebo@outlook.com>
2023-01-30 20:37:48 -0300

    distributing the home grid

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,32 @@
+import { Container, Brand, Menu, Search, Content, NewNote } from "./style"
+
+import { Header } from '../../componets/header'
+
+export function Home(){
+  return(
+    <Container>
+      <Brand>
+        <h1>RocketNotes</h1>
+      </Brand>
+
+      <Header />
+
+      <Menu>
+
+      </Menu>
+
+      <Search>
+
+      </Search>
+
+      <Content>
+
+      </Content>
+
+      <NewNote>
+
+      </NewNote>
+
+    </Container>
+  )
+}
