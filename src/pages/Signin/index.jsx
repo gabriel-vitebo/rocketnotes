e24478dee2034ed1d230b10e3173c540b5e0d3ec
Gabriel Vitebo <vitebo@outlook.com>
2023-01-31 15:59:15 -0300

    Creating the sign in page

---
+++ src/pages/Signin/index.jsx
@@ -0,0 +1,39 @@
+import { FiMail, FiLock } from "react-icons/fi"
+
+import { Input } from "../../componets/input"
+import { Button } from "../../componets/button"
+import { Container, Form, Background } from "./style"
+
+export function Signin(){
+  return (
+    <Container>
+      <Form>
+        <h1>Rocket Notes</h1>
+        <p>Aplicação para salvar e gerenciar seus links úteis.</p>
+
+        <h2>Faça seu login</h2>
+
+        <Input 
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+
+        <Input 
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Entrar" />
+
+        <a href="#">
+          Criar Conta
+        </a>
+
+      </Form>
+
+      <Background />
+    </Container>
+  )
+}
