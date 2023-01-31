b95e7612100e6a136f2b261f7c20e1f4e8c686c0
Gabriel Vitebo <vitebo@outlook.com>
2023-01-31 18:39:01 -0300

    finishing the component to create the notes

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,9 @@
+import { TextArea } from "../../componets/TextArea"
+import { NotesItem } from "../../componets/NotesItem"
+import { Section } from "../../componets/Section"
+import { Button } from "../../componets/button"
 import { Header } from "../../componets/header"
 import { Input } from "../../componets/input"
-import { TextArea } from "../../componets/TextArea"
 
 import { Container, Form } from "./style"
 
@@ -17,8 +20,20 @@ export function New() {
           </header>
 
           <Input placeholder="Titulo" />
-          <TextArea  placeholder="Observações"/>
+          <TextArea placeholder="Observações" />
 
+          <Section title="Links úteis">
+            <NotesItem value="https://rocketseat.com.br" />
+            <NotesItem value="" isNew placeholder="Novo link" />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NotesItem value="React" />
+              <NotesItem value="" isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
