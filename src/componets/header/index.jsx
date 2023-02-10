178d122ba82616ae12b8a838db4e7f69baefe6bf
Gabriel Vitebo <vitebo@outlook.com>
2023-02-09 23:13:13 -0300

    functionality to change profile picture

--- src/componets/header/index.jsx
+++ src/componets/header/index.jsx
@@ -1,19 +1,23 @@
 import { RiShutDownLine } from 'react-icons/ri'
 import { useAuth } from "../../hooks/auth"
+import { api } from "../../services/api"
+import avatarPlaceHolder from "../../assets/avatar_placeholder.svg"
 import { Container, Profile, Logout } from "./style"
 
 export function Header(){
-  const { signOut } = useAuth()
+  const { signOut, user } = useAuth()
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceHolder
+
   return (
     <Container>
       <Profile to="/profile">
-        <img
-          src="https://github.com/gabriel-vitebo.png"
-          alt="foto de perfil do usuário"
-        />
+        <img src={avatarUrl} alt={user.name} />
         <div>
           <span>Bem-Vindo</span>
-          <strong>Gabriel Vitebo</strong>
+          <strong>{user.name}</strong>
         </div>
       </Profile>
 
