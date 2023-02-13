8c0fa09b24932417d361ae6dfde9aaa89b784d1b
gabriel-vitebo <vitebo@outlook.com>
2023-02-13 11:01:00 -0300

    registering new notes

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 import { Link } from "react-router-dom"
+import { api } from "../../services/api"
+import { useNavigate } from "react-router-dom" 
 
 import { TextArea } from "../../componets/TextArea"
 import { NotesItem } from "../../componets/NotesItem"
@@ -11,12 +13,17 @@ import { Input } from "../../componets/input"
 import { Container, Form } from "./style"
 
 export function New() {
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+
 
   const [links, setLinks] = useState([])
   const [newLink, setNewLink] = useState("")
 
   const [tags, setTags] = useState([])
   const [newTag, setNewTag] = useState("")
+
+  const navigate = useNavigate()
 
   function handleAddLink(){
     setLinks(prevState => [...prevState, newLink])
@@ -36,6 +43,18 @@ export function New() {
     setTags(prevState => prevState.filter(tag => tag !== deleted))
   }
 
+  async function handleNewNote(){
+    await api.post("/notes", {
+      title,
+      description,
+      tags,
+      links
+    })
+
+    alert("Nota criada com sucesso!")
+    navigate("/")
+  }
+
   return (
     <Container>
       <Header />
@@ -47,8 +66,14 @@ export function New() {
             <Link to="/">voltar</Link>
           </header>
 
-          <Input placeholder="Titulo" />
-          <TextArea placeholder="Observações" />
+          <Input
+            placeholder="Titulo"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <TextArea
+            placeholder="Observações"
+            onChange={(e) => setDescription(e.target.value)}
+          />
 
           <Section title="Links úteis">
             {links.map((link, index) => (
@@ -86,7 +111,7 @@ export function New() {
               />
             </div>
           </Section>
-          <Button title="Salvar" />
+          <Button title="Salvar" onClick={handleNewNote} />
         </Form>
       </main>
     </Container>
