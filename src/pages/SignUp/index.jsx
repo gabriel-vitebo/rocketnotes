c5955f41ad7f1c603f025394232d9126e24b610e
gabriel-vitebo <vitebo@outlook.com>
2023-02-18 19:10:37 -0300

    adjusting the production environment

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { FiMail, FiLock, FiUser} from "react-icons/fi"
+import { FiMail, FiLock, FiUser } from "react-icons/fi"
 import { Link, useNavigate } from "react-router-dom"
 
 import { api } from "../../services/api"
@@ -8,32 +8,32 @@ import { Input } from "../../componets/input"
 import { Button } from "../../componets/button"
 import { Container, Form, Background } from "./style"
 
-export function SignUp(){
+export function SignUp() {
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
 
   const navigate = useNavigate()
 
-  function handleSignUp(){
-    if(!name || !email || !password){
+  function handleSignUp() {
+    if (!name || !email || !password) {
       return alert("Preencha todos os campos!")
     }
 
-    api.post("/users", { name, email, password})
-    .then(() => {
-      alert("Usuário cadastrado com sucesso!")
-      navigate("/")
-    })
-    .catch(error => {
-      if(error.response){
-        alert(error.response.data.message)
-      }else{
-        alert("Não foi possível cadastrar")
-      }
-    })
+    api
+      .post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso!")
+        navigate("/")
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message)
+        } else {
+          alert("Não foi possível cadastrar")
+        }
+      })
   }
-
 
   return (
     <Container>
